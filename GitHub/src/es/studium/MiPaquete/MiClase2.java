738f9cc3993126c157d193be5338b2b10d7ce40d
jevap <jevap@Jevape-PC>
2018-01-23 19:42:05 +0100

    A la nube

--- GitHub/src/es/studium/MiPaquete/MiClase2.java
+++ GitHub/src/es/studium/MiPaquete/MiClase2.java
@@ -2,12 +2,4 @@ package es.studium.MiPaquete;
 
 public class MiClase2 {
 	private int nuevoAtributo;
-
-	public int getNuevoAtributo() {
-		return nuevoAtributo;
 	}
-
-	public void setNuevoAtributo(int nuevoAtributo) {
-		this.nuevoAtributo = nuevoAtributo;
-	}
-}
